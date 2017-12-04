94ae46ee9ae206751a5bd582a3af3ba35b637c3f
Hexagon <robin@56k.guru>
2017-12-04 22:12:04 +0100

    Minor fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-mkdir /usr/src/app/external-public/
-cp -R /usr/src/app/public/* /usr/src/app/external-public/
+# /usr/src/app/external-public kan be mounted as a volume on host to expose
+# statics to be hosted by host nginx
+if [ -d /usr/src/app/external-public ]; then
+	cp -R /usr/src/app/public/* /usr/src/app/external-public/
+fi
 
 exec "$@"
